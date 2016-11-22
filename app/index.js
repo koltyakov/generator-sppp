2c2d829fe1a68ed62495ed16cb464aa723e35dd4
Andrew Koltyakov <andrew.koltyakov@arvosys.com>
2016-11-22 13:44:11 +0300

    Preinstall options

--- app/index.js
+++ app/index.js
@@ -192,6 +192,7 @@ module.exports = yeoman.Base.extend({
                 description: this.settings.appdesc,
                 main: 'index.js',
                 scripts: {
+                    preinstall: '(npm list gulp -g || npm gulp typings -g) && (npm list typings -g || npm install typings -g)',
                     test: 'echo \"Error: no test specified\" && exit 1'
                 },
                 author: this.settings.author,
