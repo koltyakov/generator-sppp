fb29d4cfb4cb988a92ed2298e850f7237ac578ca
Andrew Koltyakov <koltyakov@gmail.com>
2018-03-17 20:48:16 +0300

    Webpack conf path

--- src/templates/webpack.config.js
+++ src/templates/webpack.config.js
@@ -1,4 +1,4 @@
 // Rewrite webpack config if needed, aka "eject"
 
-const config = require('./node_modules/sp-build-tasks/dist/webpack/config');
+const config = require('sp-build-tasks/dist/webpack/config');
 module.exports = config;
