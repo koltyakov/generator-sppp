372e1f4f89d6779eea414d81b9ca076a59fe8208
Andrew Koltyakov <koltyakov@gmail.com>
2018-06-24 13:46:56 +0300

    Custom tasks and data loader strong typed

--- src/templates/build/tasks/customDataLoader.js
+++ src/templates/build/tasks/customDataLoader.js
@@ -1,7 +1,17 @@
 //@ts-check
 
-module.exports = (buildSettings, configs) => {
-  const customData = {};
-  // Load custom data object, which is passed to .hbs
-  Promise.resolve(customData);
-};
+const { customDataLoader } = require('sp-build-tasks');
+
+// Load custom data object, which is passed to templates
+
+module.exports = customDataLoader((buildSettings, configs) => {
+  return new Promise(resolve => {
+    const customData = { muData: 'my-data' };
+    resolve(customData);
+  });
+});
+
+// module.exports = (buildSettings, configs) => {
+//   const customData = {};
+//   return Promise.resolve(customData);
+// };
--- src/templates/build/tasks/example.js
+++ src/templates/build/tasks/example.js
@@ -1,10 +1,22 @@
 //@ts-check
 
-module.exports = (gulp, $, settings) => {
+const { customTask } = require('sp-build-tasks');
+
+module.exports = customTask((gulp, $, settings) => {
 
   gulp.task('example', cb => {
     console.log('Example Gulp Task');
     cb();
   });
 
-};
+});
+
+
+// module.exports = (gulp, $, settings) => {
+
+//   gulp.task('example', cb => {
+//     console.log('Example Gulp Task');
+//     cb();
+//   });
+
+// };
