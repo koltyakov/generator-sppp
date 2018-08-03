8255e3f403a3ecce5897cf82afb2a3d48be6ada0
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2018-08-03 12:38:45 +0300

    Webpack externals and define scaffolding

--- src/templates/webpack.config.js
+++ src/templates/webpack.config.js
@@ -1,4 +1,33 @@
 // Rewrite webpack config if needed, aka "eject"
 
-const config = require('sp-build-tasks/dist/webpack/config');
-module.exports = config;
+const { join } = require('path');
+const { DefinePlugin } = require('webpack');
+const configs = require('sp-build-tasks/dist/webpack/config');
+
+require('dotenv').load();
+
+const defineOptions = {
+  APP_CONFIG: JSON.stringify(
+    require(join(process.cwd(), process.env.APP_JSON || './config/app.json'))
+  )
+};
+
+configs.forEach(config => {
+
+  // Define plugin
+  config.plugins = config.plugins || [];
+  config.plugins.push(new DefinePlugin(defineOptions));
+
+  // Exclude "heavy" 3rd parties
+  config.externals = Object.assign(config.externals || {}, {
+    '@pnp/sp': 'pnp',
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'moment': 'moment',
+    'bootstrap': 'bootstrap',
+    'jquery': 'jQuery'
+  });
+
+});
+
+module.exports = configs;
