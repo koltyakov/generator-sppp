b3e1589c61a095c6aa00d9265aacdb8d1f54ab12
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-04-06 15:09:17 +0300

    loadContext helper

--- src/templates/src/scripts/index.ts
+++ src/templates/src/scripts/index.ts
@@ -1,29 +1,34 @@
 import { sp } from '@pnp/sp';
 import { setupPnp } from './utils/odata';
+import { loadContext } from './utils/env';
 
-setupPnp();
+loadContext().then(_ => {
 
-const container = document.getElementById('example-cewp-container');
+  setupPnp();
 
-if (container) {
+  const container = document.getElementById('example-cewp-container');
 
-  Promise.all([
-    sp.web.select('Title').get(),
-    sp.web.lists.select('Title').get()
-  ])
-    .then(([web, lists]) => {
-      container.innerHTML =
-        `<h2>Web title: ${web.Title}</h2>` +
-        `<ul>
-          ${lists.map(list => `<li>${list.Title}</li>`).join('')}
-        </ul>`;
-    })
-    .catch(error => {
-      container.innerHTML = `
-        <div style="color: red;">
-          ${error.message}
-        </div>
-      `;
-    });
+  if (container) {
 
-}
+    Promise.all([
+      sp.web.select('Title').get(),
+      sp.web.lists.select('Title').get()
+    ])
+      .then(([web, lists]) => {
+        container.innerHTML =
+          `<h2>Web title: ${web.Title}</h2>` +
+          `<ul>
+            ${lists.map(list => `<li>${list.Title}</li>`).join('')}
+          </ul>`;
+      })
+      .catch(error => {
+        container.innerHTML = `
+          <div style="color: red;">
+            ${error.message}
+          </div>
+        `;
+      });
+
+  }
+
+}).catch(console.warn);
