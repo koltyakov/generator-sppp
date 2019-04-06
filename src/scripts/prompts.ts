2422f12e013929b6e583aa9f01a0b5bc1d971162
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-04-06 14:34:49 +0300

    Presets and dependencies

--- src/scripts/prompts.ts
+++ src/scripts/prompts.ts
@@ -49,7 +49,10 @@ export const promptAdditionalQuestions = (_data: IGeneratorData, yo: Generator):
     name: 'additional.presets',
     message: 'Apply following presets',
     choices: [
-      { name: 'React', value: 'react' }
+      { name: 'React', value: 'react' },
+      { name: 'ESLint', value: 'eslint' },
+      { name: 'Prettier', value: 'prettier' },
+      { name: 'Editor Config', value: 'editorconfig' }
     ],
     default: yo.config.get('conf.additional.presets')
   }]) as Promise<IAnswers>;
