d92391619229b8a6047f1c9b12d28b35fc99bfd9
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-05-29 22:29:49 +0300

    Version bump

--- test/generator.spec.ts
+++ test/generator.spec.ts
@@ -45,10 +45,9 @@ describe(`SPPP tests`, () => {
           const page = await browser.newPage();
           const port = 9090; // might be dynamic in future
           const siteUrl = `http://localhost:${port}/webparts/example.cewp.html`;
-          await page.goto(siteUrl, { waitUntil: 'networkidle2' });
-
-          await page.waitForSelector(containerSelector, { timeout: 10000 });
-
+          await page.waitFor(1000);
+          await page.goto(siteUrl, { waitUntil: [ 'domcontentloaded', 'networkidle2' ] });
+          await page.waitForSelector(containerSelector);
           const content = await page.evaluate(({ containerSelector }) => {
             const c = document.querySelector(containerSelector);
             return c ? c.innerHTML : null;
